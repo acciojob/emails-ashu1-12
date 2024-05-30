1e02fc5f72900b0c0573b49a405a38ec1d75481e
ASADANSH <ashish-sanjay.sadanshiv@capgemini.com>
2024-05-30 17:21:51 +0530

    Attempt 1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -44,7 +44,7 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-		if(newPassword.length() != 8)
+		if(newPassword.length() < 8)
 			return false;
 		boolean hasUppercase=false;
 		boolean hasLowercase=false;
